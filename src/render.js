3058d3a3f3151b469531dede57673b65fd4d1bfe
김유현 <kinyuhyun@gim-yuhyeons-MacBook-Air.local>
2024-03-20 06:35:13 +0900

    feat: render.js

--- src/render.js
+++ src/render.js
@@ -4,21 +4,27 @@ export function jsx(type, props, ...children) {
 
 export function createElement(node) {
   console.log(node);
+
   // jsx를 dom으로 변환
-  if (typeof node === "string") {
-    return document.createTextNode(node);
+  const elem = document.createElement(node.type); //-> document.createElement param에 해당하는 node 생성
+
+  if (node.children) {
+    node.children.map(createElement).forEach(elem.appendChild.bind(elem)); // child 요소에 createElement 적용 후 하위요소로
   }
 
-  const $el = document.createElement(node.type);
+  if (typeof node === "string") {
+    return document.createTextNode(node); // -> tag안의 택스트를 생성
+  }
+
   if (node.props) {
+    //  node.props => { id: 'test-id', class: 'test-class' }의 구조
     Object.keys(node.props).forEach((key) => {
-      $el.setAttribute(key, node.props[key]);
+      elem.setAttribute(key, node.props[key]);
+      // tag의 속성 설정 ex) setAttribute(id, 'testId') -> <div id='testId'/>
     });
   }
-  if (node.children) {
-    node.children.map(createElement).forEach($el.appendChild.bind($el));
-  }
-  return $el;
+
+  return elem;
 }
 
 function updateAttributes(target, newProps, oldProps) {
@@ -32,34 +38,72 @@ function updateAttributes(target, newProps, oldProps) {
   //     다음 속성으로 넘어감 (속성 유지 필요)
   //   만약 newProps들에 해당 속성이 존재하지 않는다면
   //     target에서 해당 속성을 제거
+
+  for (const [attr, value] of Object.entries(newProps)) {
+    if (oldProps[attr] === newProps[attr]) continue;
+    target.setAttribute(attr, value);
+  }
+
+  for (const attr of Object.keys(oldProps)) {
+    if (newProps[attr] !== undefined) continue;
+    target.removeAttribute(attr);
+  }
 }
 
 export function render(parent, newNode, oldNode, index = 0) {
-  console.log(newNode);
+  // 1. 만약 newNode가 없고 oldNode만 있다면
+  //   parent에서 oldNode를 제거
+  //   종료
   if (!newNode && oldNode) {
     parent.removeChild(oldNode);
     return;
   }
-
+  // 2. 만약 newNode가 있고 oldNode가 없다면
+  //   newNode를 생성하여 parent에 추가
+  //   종료
   if (newNode && !oldNode) {
     const domElement = createElement(newNode);
     parent.appendChild(domElement);
     return;
   }
-
-  // 1. 만약 newNode가 없고 oldNode만 있다면
-  //   parent에서 oldNode를 제거
-  //   종료
-  // 2. 만약 newNode가 있고 oldNode가 없다면
-  //   newNode를 생성하여 parent에 추가
-  //   종료
   // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
   //   oldNode를 newNode로 교체
   //   종료
+  if (
+    typeof newNode === "string" &&
+    typeof oldNode === "string" &&
+    newNode !== oldNode
+  ) {
+    oldNode.textContent = newNode;
+    return;
+  }
   // 4. 만약 newNode와 oldNode의 타입이 다르다면
   //   oldNode를 newNode로 교체
   //   종료
+  if (typeof newNode !== typeof oldNode) {
+    const domElement = createElement(newNode);
+    parent.replaceChild(domElement, oldNode);
+    return;
+  }
   // 5. newNode와 oldNode에 대해 updateAttributes 실행
+  updateAttributes(
+    parent.childNodes[index],
+    newNode.props || {},
+    oldNode.props || {}
+  );
   // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
   //   각 자식노드에 대해 재귀적으로 render 함수 호출
+  const maxLength = Math.max(
+    newNode.children?.length,
+    oldNode.children?.length
+  );
+
+  for (let i = 0; i < maxLength; i++) {
+    render(
+      parent.childNodes[index],
+      newNode.children[i],
+      oldNode.children[i],
+      i
+    );
+  }
 }
